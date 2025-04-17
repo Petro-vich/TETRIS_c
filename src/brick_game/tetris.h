47ba8f66edf47759bb28361558b77eeaf7bcb0fc
gaylesha <petr.vasilyev.1999@gmail.com>
2025-04-17 19:56:18 +0500

    movement of the figur

--- src/brick_game/tetris.h
+++ src/brick_game/tetris.h
@@ -1,1 +1,30 @@
- 
+#ifndef TETRIC_H
+#define TETIC_H
+
+#define FIELD_WIGHT 10
+#define FIELD_HEIGHT 20
+#define SPAWN_X 4
+#define SPAWN_Y 0
+
+#include <stdio.h>
+#include <ncurses.h>
+#include <unistd.h>
+
+typedef struct
+{
+  char* status;
+  int** field;
+  int** figure;
+  int score;
+  int cd_figure_x;
+  int cd_figure_y
+} GameState_t;
+
+void init_game();
+void check_status();
+void print_matrix();
+void generate_figure();
+void move_figure();
+void move_figure();
+
+#endif
